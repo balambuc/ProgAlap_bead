af3edfcd4ab3e1fa6fb9316010b016db04ef9b9d
Bence Bardosi <bardosi.bence@gmail.com>
2016-11-29 19:26:09 +0100

    Program full kész :)
    TODO: 
     - képek dokuhoz
     - struktogram
     - doku befejezése
     - tesztesetek generálása

--- VY9NJN/main.cpp
+++ VY9NJN/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define MERET 100
 
 void beolvas(int &N, int &M, int Min[MERET], int P[MERET][MERET]);
-void megold(int N, int M, int T, int P[MERET][MERET], int Nyert[MERET], int db[MERET]);
+void megold(int N, int M, int &T, int P[MERET][MERET], int Nyert[MERET], int db[MERET]);
 int Max(int j, int N, int P[MERET][MERET]);
 int Db(int i, int N, int M, int P[MERET][MERET], int db[MERET]);
 void kiir(int T, int Gyozt[MERET]);
@@ -19,12 +19,14 @@ int main()
     int T, Nyert[MERET]= {};
 
     beolvas(N, M, Min, P);
+
     //Db fv memorizacioja
     int db[MERET];
     for(int i=0; i<N; i++)
         db[i]=-1;
+
     megold(N, M, T, P, Nyert, db);
-    //kiir(T, Nyert);
+    kiir(T, Nyert);
 
     return 0;
 }
@@ -38,9 +40,13 @@ void beolvas(int &N, int &M, int Min[MERET], int P[MERET][MERET])
         cerr << "Tanulok (N) es versenyek (M) szama szokozzel elvalasztva (N=[1..100], M=[1..100]):";
         cin >> N;
         cin >> M;
-        hiba=(N<1 || N>100) || (M<1 || M>100);
-        if(hiba)
+        hiba= cin.fail() || (cin.peek()!= '\n') || (N<1 || N>100) || (M<1 || M>100);
+        if(hiba) {
             cerr << "HIBA!" << endl;
+            cin.clear();
+            cin.ignore(100,'\n');
+        }
+
     } while(hiba);
 
     do {
@@ -48,10 +54,13 @@ void beolvas(int &N, int &M, int Min[MERET], int P[MERET][MERET])
         cerr << "A " << M << " verseny minimum ponthatarai (Mi) szokozzel elvalasztva (Mi=[0..50]):";
         for(int i=0; i<M; i++) {
             cin >> Min[i];
-            hiba=hiba || (Min[i]<0 || Min[i]>50);
+            hiba=hiba || (Min[i]<0 || Min[i]>50) || cin.fail() || (cin.peek()!= '\n');
         }
-        if(hiba)
+        if(hiba) {
             cerr << "HIBA!" << endl;
+            cin.clear();
+            cin.ignore(100,'\n');
+        }
     } while (hiba);
 
     cerr << "Kerem soronkent adja be a " << M << " verseny parametereit szokozzel elvalasztva a kovetkezo modon:" << endl;
@@ -65,21 +74,45 @@ void beolvas(int &N, int &M, int Min[MERET], int P[MERET][MERET])
             cin >> s;
             for(int j=0; j<s; j++) {
                 cin >> si;
-                hiba = hiba || si<1 || si>N;
+                hiba = hiba || si<1 || si>N || cin.fail() || (cin.peek()!= '\n');
                 cin >> P[si-1][i];
-                hiba = hiba || P[si-1][i]<0 || P[si-1][i]>100;
+                hiba = hiba || P[si-1][i]<0 || P[si-1][i]>100 || cin.fail() || (cin.peek()!= '\n');
                 if(P[si-1][i]<Min[i])
                     P[si-1][i]=0;
             }
-            if(hiba)
+            if(hiba) {
                 cerr << "HIBA!" << endl;
+                cin.clear();
+                cin.ignore(100,'\n');
+            }
         } while(hiba);
     }
 }
 
-void megold(int N, int M, int T, int P[MERET][MERET], int Nyert[MERET], int db[MERET])
+void megold(int N, int M, int &T, int P[MERET][MERET], int Nyert[MERET], int db[MERET])
 {
+    //megszamolás
+    T=0;
+    for(int i=0; i<N; i++)
+        if(Db(i,N,M,P,db)>=1)
+            T++;
 
+    //Nyert vektor inic
+    for(int i=0; i<N; i++)
+        Nyert[i]=i+1;
+
+    //buborékrendezés
+    //Egyszerre rendezi a nyert és a db halmazokat, így anyert halmazokban a tanulók indexei is "helyükre kerülnek"
+    for(int i=N-1; i>0; i--)
+        for(int j=0; j<i; j++)
+            if(db[j]<db[j+1]) {
+                int S=db[j];
+                db[j]=db[j+1];
+                db[j+1]=S;
+                S=Nyert[j];
+                Nyert[j]=Nyert[j+1];
+                Nyert[j+1]=S;
+            }
 }
 
 int Max(int j, int N, int P[MERET][MERET])
@@ -97,7 +130,7 @@ int Db(int i, int N, int M, int P[MERET][MERET], int db[MERET])
         return db[i];
     int s=0;
     for(int j=0; j<M; j++)
-        if(P[i][j]==Max(j, N, P))
+        if(P[i][j]==Max(j, N, P) && P[i][j]!=0)
             s++;
     db[i]=s;
     return s;
